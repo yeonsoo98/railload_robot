a413026ad382fa8a5b6821dc791ce38d75aa61bd
yeonsoo98 <77741178+yeonsoo98@users.noreply.github.com>
2023-02-13 11:02:20 +0900

    cmd_vel & odom add

--- railload_robot_bringup/railload_robot_bringup/driving.py
+++ railload_robot_bringup/railload_robot_bringup/driving.py
@@ -4,6 +4,7 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
 from std_msgs.msg import Int8
 
 class Driving(Node):
@@ -11,9 +12,38 @@ class Driving(Node):
         super().__init__("driving_node")
         
         self.get_logger().info("init driving node ! ")
+        
+        # robot move 
+        self.odom_publisher = self.create_publisher(
+            Odometry,
+            '/odom',
+            qos_profile)
 
+        self.odom_publisher
+        
+        # robot speed 
+        self.cmd_vel_publisher = self.create_publisher(
+            Twist,
+            '/cmd_vel',
+            qos_profile)
+            
+        self.cmd_vel_publisher
 
+        # response
+    # robot cmd_vel callback
+    def cmd_vel_callback(self, msg):
+    
+        self.info_msg.speed.linear.x = msg.linear.x
+        self.info_msg.speed.linear.y = msg.linear.y
+        self.info_msg.speed.angular.x = msg.angular.x     
 
+    # robot odom callback
+    def odom_callback(self, msg) :
+
+        self.info_msg.position.x = msg.pose.pose.position.x
+        self.info_msg.position.y = msg.pose.pose.position.y
+        self.info_msg.position.z = msg.pose.pose.position.z        
+        
 def main(args=None):
     rclpy.init(args=args)
     driving = Driving()
