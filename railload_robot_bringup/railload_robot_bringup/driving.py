836c0ef368d4a8bff17df180f2e24fddca0161a3
yeonsoo98 <yeonso981@naver.com>
2023-02-14 00:10:27 +0900

    driving.py update

--- railload_robot_bringup/railload_robot_bringup/driving.py
+++ railload_robot_bringup/railload_robot_bringup/driving.py
@@ -5,52 +5,101 @@ import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
-from std_msgs.msg import Int8
+from std_msgs.msg import Int32
+from rclpy.qos import QoSProfile
+
+import minimalmodbus as minimalmodbus
+import serial
+
+ADDRESS = 1
+
+COM_MODE = [0x01, 0x06, 0x00, 0x17, 0x00, 0x01, 0xF8, 0x02]
+SERVO_ON = [0x01, 0x06, 0x00, 0x78, 0x00, 0x01, 0xC8, 0x13]
+SERVO_OFF = [0x01, 0x06, 0x00, 0x78, 0x01, 0x00, 0x08, 0x43]
+BREAK_ON = [0x01, 0x06, 0x00, 0x78, 0x01, 0x01, 0xC9, 0x83]
+BREAK_OFF = [0x01, 0x06, 0x00, 0x78, 0x01, 0x00, 0x08, 0x43]
+RPM_P1000 = [0x01, 0x06, 0x00, 0x79, 0x03, 0xE8, 0x58, 0xAD]
+RPM_0 = [0x01, 0x06, 0x00, 0x79, 0x00, 0x00, 0x58, 0x13]
+RPM_N1000 = [0x01, 0x06, 0x00, 0x79, 0xFC, 0x18, 0x19, 0x19]
 
 class Driving(Node):
     def __init__(self):
         super().__init__("driving_node")
-        
-        self.get_logger().info("init driving node ! ")
-        
-        # robot move 
-        self.odom_publisher = self.create_publisher(
-            Odometry,
-            '/odom',
-            10)
+        qos_profile = QoSProfile(depth=10)
 
+        # motor driver setup
+        self.instrument = minimalmodbus.Instrument("/dev/motor_driver", ADDRESS)  # port name, slave address (in decimal)
+        self.instrument.serial.baudrate = 115200  # Baud
+        self.instrument.serial.bytesize = 8
+        self.instrument.serial.parity = serial.PARITY_NONE
+        self.instrument.clear_buffers_before_each_transaction = True
+        self.instrument.serial.stopbits = 1
+        self.instrument.serial.timeout = 3  # seconds
+        self.instrument.mode = minimalmodbus.MODE_RTU  # rtu or ascii mode
 
-        self.odom_publisher
-        
-        # robot speed 
-        self.cmd_vel_publisher = self.create_publisher(
+        try:
+            self.instrument.serial.write(bytes(COM_MODE))
+            self.instrument.serial.write(bytes(SERVO_ON))
+            self.instrument.serial.write(bytes(BREAK_OFF))
+            self.instrument.serial.write(bytes(RPM_0))
+        except Exception as e:
+            self.get_logger().error("Fail to initialize : {}".format(e))
+            self.destroy_node()
+            rclpy.shutdown()
+
+        # subscriber
+        self.cmd_vel_sub = self.create_subscription(
             Twist,
-            '/cmd_vel',
-            10)
-            
-        self.cmd_vel_publisher
+            'cmd_vel',
+            self.cmd_vel_callback,
+            10
+        )
 
-        # response
+        # publisher
+        self.req_rpm_publisher = self.create_publisher(Int32, 'req_rpm', qos_profile)
+        self.cur_rpm_publisher = self.create_publisher(Int32, 'cur_rpm', qos_profile)
+
+        # timer
+        timer_period= 0.2 # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+
+        self.get_logger().info("init driving node ! ")
+
+
+    # info timer
+    def timer_callback(self):
+        # publish current rpm
+        cur_rpm = Int32()
+        cur_rpm.data = self.instrument.read_register(registeraddress=0x03, number_of_decimals=0, functioncode=4, signed=False)
+        self.cur_rpm_publisher.publish(cur_rpm)
+
+        # publish required rpm
+        req_rpm = Int32()
+        req_rpm.data = self.instrument.read_register(registeraddress=0x02, number_of_decimals=0, functioncode=4, signed=False)
+        self.req_rpm_publisher.publish(req_rpm)
+
     # robot cmd_vel callback
     def cmd_vel_callback(self, msg):
-    
-        self.info_msg.speed.linear.x = msg.linear.x
-        self.info_msg.speed.linear.y = msg.linear.y
-        self.info_msg.speed.angular.x = msg.angular.x     
+        try:
+            if msg.linear.x > 0.0:
+                self.instrument.serial.write(bytes(RPM_P1000))
+            elif msg.linear.x < 0.0:
+                self.instrument.serial.write(bytes(RPM_N1000))
+            else:
+                self.instrument.serial.write(bytes(RPM_0))
+        except Exception as e:
+            self.get_logger().error("Fail to write : {}".format(e))
 
-    # robot odom callback
-    def odom_callback(self, msg) :
-
-        self.info_msg.position.x = msg.pose.pose.position.x
-        self.info_msg.position.y = msg.pose.pose.position.y
-        self.info_msg.position.z = msg.pose.pose.position.z        
-        
 def main(args=None):
     rclpy.init(args=args)
     driving = Driving()
     rclpy.spin(driving)
+
+    # Destroy the node explicitly
+    driving.instrument.serial.close()
     driving.destroy_node()
     rclpy.shutdown()
+
 
 if __name__=='__main__':
     main()
